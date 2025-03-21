184c3dc8c9ff98b72b9937bd3a291d00f7ad1d47
shinzi <shinzi0427@gmail.com>
2025-03-21 23:18:15 +0800

    Update route.ts

--- src/app/api/gateway/route.ts
+++ src/app/api/gateway/route.ts
@@ -1,11 +1,5 @@
-import {createGatewayOnEdgeRuntime} from "@lobehub/chat-plugins-gateway";
-import {NextRequest, NextResponse} from "next/server";
+import { NextResponse } from 'next/server';
 
-export async function OPTIONS(req: NextRequest, res: NextResponse) {
-	return createGatewayOnEdgeRuntime()(req)
-}
-
-export async function POST(req: Request) {
-	console.log('in gateway', process.env.NODE_ENV)
-	return createGatewayOnEdgeRuntime()(req)
+export async function GET(request: Request) {
+  return NextResponse.json({ message: 'Hello from xAI Image Plugin!' });
 }
